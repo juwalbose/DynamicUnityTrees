c13226044f0d2ad8bd0be85bc0d03ed80b3483f9
Juwal Bose <juwalbose@gmail.com>
2018-04-03 22:32:53 +0300

    L system tree implemented

--- Unity Tree Experiments/Assets/Scripts/LSystemTree.cs
+++ Unity Tree Experiments/Assets/Scripts/LSystemTree.cs
@@ -1,32 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class LSystemTree : MonoBehaviour {
 
 	List<Branch> branches;
-	bool isDebug=true;
+	bool isDebug=false;
 	Mesh lTree;
 	List<int> faces;
 	List<Vector3> vertices;
 
 	public string[] vars;
-	[SerializeField]
-	public Dictionary<string,string> rules;
-	public string lString;
+	Dictionary<string,string> rules;
+	string lString;
+	public int iterations=3;
+	public int angle=25;
+	public float startLength=2;
+	public float startWidth=1;
+
+	List<LevelState> levelStates;
+	public Vector3 treeOrigin;
+	public float widthDecreaseFactor;
+	public float lengthDecreaseFactor;
 	
 
 	// Use this for initialization
 	void Start () {
-		//rules=new Dictionary<string, string>();
-		lString=IterateLString(lString);
+		lString=vars[0];
+		rules=new Dictionary<string, string>();
+		rules[vars[0]]="FF+[+F-F-F]-[-F+F+F]";
+		for(int i=0;i<iterations;i++){
+			lString=IterateLString(lString);
+		}
+		
+		
+		//lString="F[+F[+F]-F]-F[+F]-F";
+		//lString="F[+F]--F";
 		Debug.Log(lString);
 
 		branches=new List<Branch>();
 		faces=new List<int>();
 		vertices=new List<Vector3>();
-		//lTree=GetComponent<MeshFilter>().mesh;
-		//lTree.name="l system tree";
+		lTree=GetComponent<MeshFilter>().mesh;
+		lTree.name="l system tree";
 		CreateTree();
 	}
 	string IterateLString(string currentLString){
@@ -43,13 +59,98 @@ public class LSystemTree : MonoBehaviour {
 		return newLString;
 	}
 	private void CreateTree(){
+		levelStates=new List<LevelState>();
+		char[] chars=lString.ToCharArray();
+		float currentRotation=0;
+		float currentLength=startLength;
+		float currentWidth=startWidth;
+		Vector3 currentPosition=treeOrigin;
+		int levelIndex=0;
+		LevelState levelState=new LevelState();
+		levelState.position=currentPosition;
+		levelState.levelIndex=levelIndex;
+		levelState.width=currentWidth;
+		levelState.length=currentLength;
+		levelState.rotation=currentRotation;
+		levelState.logicBranches=new List<LevelState.BranchState>();
+		levelStates.Add(levelState);
+		Vector3 tipPosition=new Vector3();
+		Queue<LevelState> savedStates=new Queue<LevelState>();
+		for(int i=0;i<chars.Length;i++){
+			switch(chars[i]){
+				case 'F':
+					if(levelState.levelIndex!=levelIndex){
+						foreach(LevelState ls in levelStates){
+							if(ls.levelIndex==levelIndex){
+								levelState=ls;
+								break;
+							}
+						}	
+					}
+					tipPosition.x=levelState.position.x+(currentLength*Mathf.Sin(Mathf.Deg2Rad*currentRotation));
+					tipPosition.y=levelState.position.y+(currentLength*Mathf.Cos(Mathf.Deg2Rad*currentRotation));
+					levelIndex++;
+					LevelState.BranchState branchState=new LevelState.BranchState();
+					branchState.rotation=currentRotation;
+					branchState.length=currentLength;
+					levelState.logicBranches.Add(branchState);
+
+					currentWidth*=widthDecreaseFactor;
+
+					currentPosition=tipPosition;
+					levelState=new LevelState();
+					levelState.position=currentPosition;
+					levelState.levelIndex=levelIndex;
+					levelState.width=currentWidth;
+					levelState.length=currentLength;
+					levelState.rotation=currentRotation;
+					levelState.logicBranches=new List<LevelState.BranchState>();
+					levelStates.Add(levelState);
+
+					currentLength*=lengthDecreaseFactor;
+					
+				break;
+				case '+':
+					currentRotation+=angle;
+				break;
+				case '-':
+					currentRotation-=angle;
+				break;
+				case '[':
+					savedStates.Enqueue(levelState);
+				break;
+				case ']':
+					levelState=savedStates.Dequeue();
+					currentPosition=levelState.position;
+					currentRotation=levelState.rotation;
+					currentLength=levelState.length;
+					currentWidth=levelState.width;
+					levelIndex=levelState.levelIndex;
+				break;
+			}
+		}
 		
-		/*
+		for(int i=0;i<levelStates.Count;i++){
+			levelState=levelStates[i];
+			foreach(LevelState.BranchState bs in levelState.logicBranches){
+				Branch branch=new Branch(Vector3.zero,bs.length,levelState.width,bs.rotation, levelState.position,widthDecreaseFactor);
+				branches.Add(branch);
+				int baseVertexPointer=vertices.Count;
+				vertices.AddRange(branch.vertices);
+				faces.Add(baseVertexPointer);
+				faces.Add(baseVertexPointer+1);
+				faces.Add(baseVertexPointer+3);
+				faces.Add(baseVertexPointer+3);
+				faces.Add(baseVertexPointer+1);
+				faces.Add(baseVertexPointer+2);
+			}
+		}
+		 
 		lTree.vertices=vertices.ToArray();
 		lTree.triangles = faces.ToArray();
 		lTree.RecalculateNormals();
-		Debug.Log("Tree has "+vertices.Count.ToString()+" vertices");
-		*/
+		//Debug.Log("Tree has "+vertices.Count.ToString()+" vertices");
+		
 	}
 
 	private void OnDrawGizmos () {
@@ -60,7 +161,7 @@ public class LSystemTree : MonoBehaviour {
 		Gizmos.color = Color.green;
 		foreach(Branch br in branches){
 			for (int i = 0; i < br.vertices.Count; i++) {
-				Gizmos.DrawSphere(br.vertices[i], 0.1f);
+				Gizmos.DrawSphere(br.vertices[i], 0.05f);
 			}
 		}
 		
---
+++ Unity Tree Experiments/Assets/Scripts/components/LevelState.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelState  {
+
+	public struct BranchState{
+		public float rotation;
+		public float length;
+		
+	}
+	public int levelIndex;
+	public float width;
+	public Vector3 position;
+	public float rotation;
+	public float length;
+	public List<BranchState> logicBranches;
+}
